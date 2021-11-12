579bffc731b6bdcd9551b9f7cd4e7ceff743a877
Raphaël Calabro <ddaeke-github@yahoo.fr>
2021-11-12 16:34:50 +0100

    Reading headers + Initial support for quoted-printable content transfer encoding.

--- src/main/java/com/github/raphcal/mailbank/MailBuilder.java
+++ src/main/java/com/github/raphcal/mailbank/MailBuilder.java
@@ -4,12 +4,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -21,9 +29,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MailBuilder {
     private static final String LINE_ENDING = "\r\n";
+    private static final String CONTENT_TRANSFER_ENCODING_HEADER = "Content-Transfer-Encoding";
+    private static final String QUOTED_PRINTABLE_CONTENT_TRANSFER_ENCODING = "quoted-printable";
 
     private Status status = Status.INITIAL;
-    private final StringBuilder lineBuilder = new StringBuilder();
     private final ByteArrayOutputStream dataBuilder = new ByteArrayOutputStream();
     private String response;
 
@@ -31,6 +40,9 @@ public class MailBuilder {
     private String client;
     private String from;
     private List<String> to = new ArrayList<>();
+    private Map<String, String> headers = new LinkedHashMap<>();
+
+    private Charset charset = StandardCharsets.US_ASCII;
 
     public MailBuilder(InetSocketAddress endpoint) {
         hostName = endpoint.getHostName();
@@ -41,7 +53,24 @@ public class MailBuilder {
         String line = readLine(buffer);
         while (line != null) {
             log.trace("< " + line);
-            if (status != Status.BODY) {
+            if (status == Status.HEADERS) {
+                if (line.isEmpty()) {
+                    status = Status.BODY;
+                } else if(line.contains(":")) {
+                    String[] headerAndValue = line.split(": *", 2);
+                    headers.put(headerAndValue[0], headerAndValue[1]);
+
+                    Pattern charsetPattern = Pattern.compile("charset=([^;]+)");
+                    Matcher matcher;
+                    if ("Content-Type".equals(headerAndValue[0]) && (matcher = charsetPattern.matcher(headerAndValue[1])).find()) {
+                        charset = Charset.forName(matcher.group(1));
+                    }
+                } else {
+                    dataBuilder.write(line.getBytes(charset));
+                    dataBuilder.write('\r');
+                    dataBuilder.write('\n');
+                }
+            } else if (status != Status.BODY) {
                 final String[] args = line.split("[ :]");
 
                 final Command command = Command.valueOf(args[0].toUpperCase());
@@ -81,7 +110,7 @@ public class MailBuilder {
                                 response = "250 Recipient OK" + LINE_ENDING;
                                 break;
                             case DATA:
-                                status = Status.BODY;
+                                status = Status.HEADERS;
                                 response = "354 Enter mail, end with '.' on a line by itself" + LINE_ENDING;
                                 break;
                             default:
@@ -94,7 +123,7 @@ public class MailBuilder {
                     status = Status.DONE;
                     response = "250 Data OK";
                 } else {
-                    dataBuilder.write(line.getBytes(StandardCharsets.US_ASCII));
+                    dataBuilder.write(line.getBytes(charset));
                     dataBuilder.write('\r');
                     dataBuilder.write('\n');
                 }
@@ -118,18 +147,47 @@ public class MailBuilder {
     }
 
     public Mail build() {
-        return new Mail(from, to, null, null, null, dataBuilder.toString());
+        String data = dataBuilder.toString();
+        if (QUOTED_PRINTABLE_CONTENT_TRANSFER_ENCODING.equalsIgnoreCase(headers.get(CONTENT_TRANSFER_ENCODING_HEADER))) {
+            Pattern specialChars = Pattern.compile("(?:=[A-Za-z0-9]{2})+");
+            Matcher matcher = specialChars.matcher(data);
+            while (matcher.find()) {
+                final int[] integers = Arrays.stream(matcher.group().substring(1).split("="))
+                        .mapToInt(hex -> Integer.parseInt(hex, 16))
+                        .toArray();
+                final byte[] bytes = new byte[integers.length];
+                for (int index = 0; index < integers.length; index++) {
+                    bytes[index] = (byte)integers[index];
+                }
+                try {
+                    final String decodedValue = charset.newDecoder().decode(ByteBuffer.wrap(bytes)).toString();
+                    data = data.replace(matcher.group(), decodedValue);
+                    matcher = specialChars.matcher(data);
+                } catch (CharacterCodingException ex) {
+                    log.error("Unable to decode bytes " + Arrays.toString(bytes) + " using charset " + charset.displayName(), ex);
+                }
+            }
+
+            specialChars = Pattern.compile("=([A-Za-z0-9]{2})");
+            matcher = specialChars.matcher(data);
+            while (matcher.find()) {
+                data = data.replace(matcher.group(), "" + ((char)Integer.parseInt(matcher.group(1), 16)));
+                matcher = specialChars.matcher(data);
+            }
+            data = data.replace("=\n", "").replace("=\r\n", "");
+        }
+        
+        return new Mail(from, to, null, null, headers, data);
     }
 
     private String readLine(ByteBuffer buffer) {
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         while (buffer.hasRemaining()) {
             char c = (char) buffer.get();
             if (c != '\r' && c != '\n') {
-                lineBuilder.append(c);
+                outputStream.write(c);
             } else if (c == '\n') {
-                final String line = lineBuilder.toString();
-                lineBuilder.setLength(0);
-                return line;
+                return new String(outputStream.toByteArray(), charset);
             }
         }
         return null;
@@ -140,6 +198,7 @@ public class MailBuilder {
         INITIAL(Command.HELO, Command.EHLO, Command.QUIT),
         REQUIRE_AUTH(Command.AUTH, Command.QUIT),
         ENVELOPE(Command.MAIL, Command.RCPT, Command.DATA, Command.QUIT),
+        HEADERS,
         BODY,
         WAITING_FOR_QUIT(Command.QUIT),
         DONE;
